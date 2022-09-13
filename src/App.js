31db013db3f2379480ad57e38950d9a5397611f2
syuulee <yuyu00lee@gmail.com>
2022-09-13 12:43:34 +0900

    2022.09.13.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './component/Header';
+
+const App = () => {
+
+  return (
+    <div className="Wrap">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
